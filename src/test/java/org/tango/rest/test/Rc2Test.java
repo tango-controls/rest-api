7c187b8a8a5c3fd72c9ddad87cc8bac34c4efaab
Ingvord <tla-ingvord@yandex.ru>
2016-01-30 20:11:00 +0300

    add Rc2Test

---
+++ src/test/java/org/tango/rest/test/Rc2Test.java
@@ -0,0 +1,46 @@
+package org.tango.rest.test;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.fail;
+
+/**
+ * @author ingvord
+ * @since 1/30/16
+ */
+public class Rc2Test extends Rc1Test {
+    @Override
+    protected String getVersion() {
+        return "rc2";
+    }
+
+    @Test
+    public void testPutAttributeProperty(){
+        //TODO
+        fail();
+    }
+
+    @Test
+    public void testDeleteAttributeProperty(){
+        //TODO
+        fail();
+    }
+
+    @Test
+    public void testPutAttributeInfo(){
+        //TODO
+        fail();
+    }
+
+    @Test
+    public void testGetAttributeHistory(){
+        //TODO
+        fail();
+    }
+
+    @Test
+    public void testGetCommandHistory(){
+        //TODO
+        fail();
+    }
+}
