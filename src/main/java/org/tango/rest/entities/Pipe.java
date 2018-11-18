466ca2204745ce01697c5f1543d6f3ac8e98b889
ingvord <tla-ingvord@yandex.ru>
2018-11-19 01:13:00 +0300

    Progress:

--- src/main/java/org/tango/rest/entities/Pipe.java
+++ src/main/java/org/tango/rest/entities/Pipe.java
@@ -1,6 +1,10 @@
 package org.tango.rest.entities;
 
+import fr.esrf.Tango.DevError;
 import fr.esrf.TangoApi.PipeInfo;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.net.URI;
 
 /**
  * @author Igor Khokhriakov <igor.khokhriakov@hzg.de>
@@ -11,6 +15,20 @@ public class Pipe {
     public String name;
     public String device;
     public String host;
-    public String value;
     public PipeInfo info;
+    public String value;
+    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+    public DevError[] errors;
+
+    public Pipe() {
+    }
+
+    public Pipe(String name, String device, String host, PipeInfo info, URI href) {
+        this.id = host + "/" + device + "/" + name;
+        this.name = name;
+        this.device = device;
+        this.host = host;
+        this.info = info;
+        this.value = href + "/value";
+    }
 }
